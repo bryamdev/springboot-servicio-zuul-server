fde35f6a4e4a66dbff6c96f1ee6d2e9b3bb57d00
Bryam Castaneda <bryamdev.contact@gmail.com>
2022-10-24 21:45:27 -0500

    agregando configuración CORS en spring security y en global

--- src/main/java/com/micro/springboot/app/zuul/oauth/ResourceServerConfig.java
+++ src/main/java/com/micro/springboot/app/zuul/oauth/ResourceServerConfig.java
@@ -1,9 +1,13 @@
 package com.micro.springboot.app.zuul.oauth;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -11,6 +15,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @RefreshScope
 @Configuration
@@ -26,7 +34,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 		resources.tokenStore(tokenStore());
 	}
 	
-	//Configurando las reglas de autorizacion para los endpoints de los servicios ruteados.
+	//Configurando las reglas de autorizacion en Spring Security para los endpoints de los servicios ruteados.
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
@@ -42,7 +50,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 					"/api/v1/items/editar/{id}", "/api/v1/usuarios/usuarios/{id}").hasAnyRole("ADMIN")
 			.antMatchers(HttpMethod.DELETE, "/api/v1/productos/eliminar/{id}",
 					"/api/v1/items/eliminar/{id}", "/api/v1/usuarios/usuarios/{id}").hasAnyRole("ADMIN")
-			.anyRequest().authenticated();
+			.anyRequest().authenticated()
+			.and().cors().configurationSource(corsConfigurationSource());
 		
 	}
 
@@ -57,5 +66,30 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 		accessTokenConverter.setSigningKey(jwtKey);
 		return accessTokenConverter;
 	}	
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.addAllowedOrigin("*");
+		corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		corsConfig.setAllowCredentials(true);
+		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+		
+		//UrlBasedCorsConfigurationSource: clase para configurar rutas con la config de cors realizada
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+		
+		return source;
+	}
+	
+	//Bean que se encarga de configurar los cors de forma global, no solo en spring security
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+		FilterRegistrationBean<CorsFilter> bean = 
+				new FilterRegistrationBean<CorsFilter>(new CorsFilter(corsConfigurationSource())); 
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
+	
 	
 }
